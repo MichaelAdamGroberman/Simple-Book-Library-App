e1727c5ed15e0261d4216410736adbd91570e2d8
Michael Adam Groberman <michael.groberman@me.com>
2021-11-17 02:22:37 -0500

    please

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mgroberman:Tu3sd2y29@cluster0.toizs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mgroberman:Tu3sd2y29@cluster0.toizs.mongodb.net/book_archive?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
