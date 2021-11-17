c4f3676ca2e9c6de60d6d9a0c8688ee2fa300643
Michael Adam Groberman <michael.groberman@me.com>
2021-11-17 03:50:27 -0500

    praying now

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import Navbar from './component/Navigation';
 
 const client = new ApolloClient({
   request: operation => {
@@ -24,7 +24,7 @@ function App() {
     <ApolloProvider client={client} className="app-background-color">
       <Router>
         <>
-          <Navbar />
+          <Navigation/>
           <Switch className="app-background-color">
             <Route exact path='/' component={SearchBooks} />
             <Route exact path='/saved' component={SavedBooks} />
